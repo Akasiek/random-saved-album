e5f3bb628ed05ede58fc37a67f40bdda239d253d
Akasiek <akasiek999@o2.pl>
2020-07-23 20:06:38 +0200

    Changing algorithm. Big performence boost

--- randomSavedAlbum.py
+++ randomSavedAlbum.py
@@ -12,24 +12,19 @@ spotify = tk.Spotify(token)
 albums_number = spotify.saved_albums(limit=1).total
 albums = {}
 
+# Checking if the user has any saved albums
 if albums_number == 0:
-    print("You don't have any saved albums")
+    print("You don't have any saved albums. Aborting!")
     sleep(2)
-
 else:
-    # Appending all albums into the tabel
-    # Sleep is for not getting a limit error
-    for album in range(albums_number):
-        albums[album] = spotify.saved_albums(
-            limit=1, offset=album).items[0].album
-        sleep(0.02)
-
-    # Getting random number and selecting the album by this number
-    random_number = random.randint(1, albums_number)
-    selected_album = albums[random_number]
+    # Getting random number and selecting the album
+    random_number = random.randint(0, albums_number-1)
+    selected_album = spotify.saved_albums(
+        limit=1, offset=random_number).items[0].album
 
     # Playing the selected album
     print(
-        f"Album has been selected. Now playing '{selected_album.name}' by {selected_album.artists[0].name} on Spotify")
+        f"Album has been selected! Now playing '{selected_album.name}' by {selected_album.artists[0].name} on Spotify")
+
     selected_album_uri = tk.to_uri('album', selected_album.id)
     spotify.playback_start_context(selected_album_uri)
